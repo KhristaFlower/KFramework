93ec3a72657d415268228e8c2ed10e7cacb05f05
Kriptonic <christopher.sharman@live.co.uk>
2013-05-15 01:33:03 +0100

    A few small changes were made to the files as well as the creation of some KFramework functions.

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?
 
+set_include_path(__DIR__."/");
+
 // Load the configuration
 require 'config.php';
+
+// Autoload the libraries
+function __autoload($class)
+{
+    require "libs/$class.php";
+}
+
+$KFramework = new KFramework();
+$KFramework->init();
 
 ?>
--- libs/KFramework.php
+++ libs/KFramework.php
@@ -1,7 +1,7 @@
 <?php
 
-class KFramework
-{
+class KFramework {
+
     private $_url = null;
     private $_controller = null;
     private $_controllerPath = PATH_CONTROLLERS;
@@ -9,6 +9,70 @@ class KFramework
     private $_viewPath = PATH_VIEWS;
     private $_defaultErrorController = SITE_DEFAULT_ERROR_CONTROLLER;
     private $_defaultController = SITE_DEFAULT_CONTROLLER;
+
+    /**
+     * Start the framework.
+     */
+    public function init() {
+        $this->_getURL();
+        $this->_loadController();
+        //$this->_callControllerMethod();
+    }
+
+    /**
+     * Process the URL into useable segments.
+     */
+    private function _getURL() {
+        $url = isset($_GET['url']) ? $_GET['url'] : $this->_defaultController;
+        $url = rtrim($url, '/');
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        $this->_url = explode('/', $url);
+    }
+
+    /**
+     *  Load the controller which matches the request in the URL.
+     */
+    private function _loadController() {
+        $controller = $this->_selectController();
+        if (empty($controller)){
+            echo "No controller found.";
+            die();
+        }
+        
+        echo $controller;
+        die();
+    }
+
+    /**
+     *  Calculate the controller that should be loaded using the URL.
+     * 
+     *  The following checks will be performed in the controller folder until a controller is located:
+     *  url[0].php as a controller
+     *  url[0]/url[1].php as a controller
+     *  url[0]/url[0].php as a controller
+     *  Throw a 404, we were unable to locate a controller.
+     * 
+     */
+    private function _selectController() {
+        $possibleController = $this->_controllerPath . $this->_url[0] . '.php';
+        if (file_exists($possibleController)) {
+            return $possibleController;
+        }
+
+        $possibleController = $this->_controllerPath . "{$this->_url[0]}/{$this->_url[1]}.php";
+        if (file_exists($possibleController)) {
+            return $possibleController;
+        }
+        
+        $possibleController = $this->_controllerPath . "{$this->_url[0]}/{$this->_url[0]}.php";
+        if (file_exists($possibleController)){
+            return $possibleController;
+        }
+        
+        // No controller was found in a location that it could be expected based on the URL parameters.
+        return null;
+    }
+
 }
 
 ?>
